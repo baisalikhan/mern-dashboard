7c254e074b899e68cdc9c433bb142cef2f9a17ee
baisexodevs <112929397+baisalikhan@users.noreply.github.com>
2025-04-27 16:35:56 +0500

    fix: change the folder name

--- client/src/scenes/breakdown/index.jsx
+++ client/src/scenes/breakdown/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Box } from "@mui/material";
 import Header from "components/Header";
-import BreakdownChart from "components/BreakdownChart";
+import BreakdownChart from "components/BreakDownChart";
 
 const Breakdown = () => {
   return (
